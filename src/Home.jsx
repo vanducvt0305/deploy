88c4b8ff7a55700348bda0ff3f888cf8a84f1e60
DucNguyen <vanducvt0305@gmail.com>
2022-10-19 20:10:22 +0700

    fix

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div>
+        <Link to={'product'}>
+        Qua Product
+        </Link>
+    </div>
+  )
+}
